f91e75ebb22841a1e310eca2ee242c1c7e687a90
jassirahmed <jassirahmed6@gmail.com>
2022-05-25 14:44:21 +0530

    Alignments

--- Components/FormValidation.tsx
+++ Components/FormValidation.tsx
@@ -1,35 +1,40 @@
 import React from "react";
+import { Box, Button } from "@chakra-ui/react";
 import {
-  Formik,
-  FormikHelpers,
-  FormikProps,
-  Form,
-  Field,
-  FieldProps,
+    Formik,
+    FormikHelpers,
+    FormikProps,
+    Form,
+    Field,
+    FieldProps,
 } from "formik";
 
 interface MyFormValues {
-  firstName: string;
+    firstName: string;
 }
 export default function FormValidation() {
-  const initialValues: MyFormValues = { firstName: "" };
-  return (
-    <div>
-      <h1>My Example</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
-      >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="First Name" />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
-  );
+    const initialValues: MyFormValues = { firstName: "" };
+    return (
+        <Box>
+            <Box as="h1" textAlign="center">Validation Form</Box>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => {
+                    console.log({ values, actions });
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                }}
+            >
+                <Form>
+                    <Box textAlign="center">
+                        <label htmlFor="firstName">First Name</label>
+                        <Field id="firstName" name="firstName" placeholder="First Name" />
+                    </Box>
+                    <Box textAlign={"center"}>
+                        <button>Submit</button>
+                    </Box>
+                </Form>
+            </Formik>
+        </Box>
+    );
 }
