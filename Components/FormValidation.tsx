4074f0ebd19e85b0c24c8065a06575073140c101
Safwan Ahmed <a.safwan@ilmux.com>
2022-05-25 14:31:10 +0530

    formik code

--- Components/FormValidation.tsx
+++ Components/FormValidation.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
 
+interface MyFormValues {
+  firstName: string;
+}
 export default function FormValidation() {
-    return (
-        <div>Hello</div>
-    )
+  const initialValues: MyFormValues = { firstName: "" };
+  return (
+    <div>
+      <h1>My Example</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions });
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="First Name" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
